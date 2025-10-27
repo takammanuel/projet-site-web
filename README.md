6935d376afd91250086d845ad2e20ba5b14e4f41
takam manuel <takammanuel5@gmail.com>
2025-10-27 15:30:58 +0100

    Nettoyage avant fusion et ajout de mes fichiers

--- README.md
+++
@@ -1,2 +0,0 @@
-# projet-site-web
-site web collaboratif avec 3 personne
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
   const services = document.querySelectorAll(".service");
   services.forEach((s, i) => {
@@ -7,4 +8,12 @@ document.addEventListener("DOMContentLoaded", () => {
       s.style.opacity = 1;
     }, i * 400);
   });
+=======
+// Petit effet sur le bouton d’accueil
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btn-info");
+    btn.addEventListener("click", () => {
+        alert("Merci ! Un agent prendra contact avec vous pour planifier votre rendez-vous.");
+    });
+>>>>>>> a59e1cc (Nettoyage avant fusion et ajout de mes fichiers)
 });
